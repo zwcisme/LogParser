1c8d197dbeb3c90e648d24381652831b1fdfa48a
Simon <zwcisme@gmail.com>
2014-10-30 16:28:26 +0800

    logparser use Runtime

--- src/com/xiaoju/qa/tools/logParser/parser/LogParser.java
+++ src/com/xiaoju/qa/tools/logParser/parser/LogParser.java
@@ -1,5 +1,36 @@
 package com.xiaoju.qa.tools.logParser.parser;
 
-public class LogParser {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class LogParser  {
+
+	public static void main(String[] args) {
+		Runtime runtime = Runtime.getRuntime();
+		Process process = null;
+		String line = null;
+		InputStream is = null;
+		InputStreamReader isr = null;
+		BufferedReader br = null;
+		try {
+			process = runtime.exec(" tail -f ./result.log ");
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		is = process.getInputStream();
+		isr = new InputStreamReader(is);
+		br = new BufferedReader(isr);
+		try {
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+
+	}
 
 }
